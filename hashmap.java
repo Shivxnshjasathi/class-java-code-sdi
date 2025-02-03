8d8346b2f444683c62032753e01f24593556da9d
Shivansh jasathi <116382647+Shivxnshjasathi@users.noreply.github.com>
2025-02-03 10:36:14 +0530

    first commit

---
+++ hashmap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+class hashex {
+    public static void main (String [] args){
+        Map <String,Integer> temp = new HashMap<>();
+        temp.put("BBSR",50);
+        temp.put("CTC",50);
+        System.out.println(temp.get("BBSR"));
+
+    }
+}
